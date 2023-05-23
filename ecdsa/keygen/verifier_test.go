cfefbbe28969445a6b73e973d3789f3227ab49ec
Promethea Raschke <promethea.raschke@keep.network>
2023-05-23 14:58:50 +0100

    Test ParamProof verifier

--- ecdsa/keygen/verifier_test.go
+++ ecdsa/keygen/verifier_test.go
@@ -303,3 +303,110 @@ func prepareProof() (*LocalPreParams, [][]byte, [][]byte, error) {
 
 	return &preParams, common.BigIntsToBytes(proof.Alpha[:]), common.BigIntsToBytes(proof.T[:]), nil
 }
+
+func TestVerifyParamProof_Success(t *testing.T) {
+	preParams, a, z, s, tt := prepareParamProofT(t)
+	message := &KGRound1Message{
+		Prmproof: &KGRound1Message_ParamProof{
+			A: a,
+			Z: z,
+		},
+	}
+
+	verifier := NewProofVerifier(runtime.GOMAXPROCS(0))
+
+	resultChan := make(chan bool)
+
+	verifier.VerifyParamProof(message, preParams.PaillierSK.PublicKey.N, s, tt, func(result bool) {
+		resultChan <- result
+	})
+
+	success := <-resultChan
+	if !success {
+		t.Fatal("expected positive verification")
+	}
+}
+
+func TestVerifyParamProof_MalformedMessage1(t *testing.T) {
+	preParams, a, z, s, tt := prepareParamProofT(t)
+	message := &KGRound1Message{
+		Prmproof: &KGRound1Message_ParamProof{
+			A: a[:len(a)-1],
+			Z: z,
+		},
+	}
+
+	verifier := NewProofVerifier(runtime.GOMAXPROCS(0))
+
+	resultChan := make(chan bool)
+
+	verifier.VerifyParamProof(message, preParams.PaillierSK.PublicKey.N, s, tt, func(result bool) {
+		resultChan <- result
+	})
+
+	success := <-resultChan
+	if success {
+		t.Fatal("expected negative verification")
+	}
+}
+
+func TestVerifyParamProof_MalformedMessage2(t *testing.T) {
+	preParams, a, z, s, tt := prepareParamProofT(t)
+	message := &KGRound1Message{
+		Prmproof: &KGRound1Message_ParamProof{
+			A: a,
+			Z: z[:len(z)-1],
+		},
+	}
+
+	verifier := NewProofVerifier(runtime.GOMAXPROCS(0))
+
+	resultChan := make(chan bool)
+
+	verifier.VerifyParamProof(message, preParams.PaillierSK.PublicKey.N, s, tt, func(result bool) {
+		resultChan <- result
+	})
+
+	success := <-resultChan
+	if success {
+		t.Fatal("expected negative verification")
+	}
+}
+
+func prepareParamProofT(t *testing.T) (*LocalPreParams, [][]byte, [][]byte, *big.Int, *big.Int) {
+	preParams, a, z, s, tt, err := prepareParamProof()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return preParams, a, z, s, tt
+}
+
+func prepareParamProofB(b *testing.B) (*LocalPreParams, [][]byte, [][]byte, *big.Int, *big.Int) {
+	preParams, a, z, s, t, err := prepareParamProof()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	return preParams, a, z, s, t
+}
+
+func prepareParamProof() (*LocalPreParams, [][]byte, [][]byte, *big.Int, *big.Int, error) {
+	localPartySaveData, _, err := LoadKeygenTestFixtures(1)
+	if err != nil {
+		return nil, [][]byte{}, [][]byte{}, nil, nil, err
+	}
+
+	preParams := localPartySaveData[0].LocalPreParams
+
+	N := preParams.PaillierSK.PublicKey.N
+	modN := common.ModInt(N)
+	lambda := common.GetRandomPositiveInt(preParams.PaillierSK.PhiN)
+	r := common.GetRandomPositiveRelativelyPrimeInt(N)
+	t := modN.Mul(r, r)
+	s := modN.Exp(t, lambda)
+
+	proof := preParams.PaillierSK.ParamProof(s, t, lambda)
+
+	return &preParams, common.BigIntsToBytes(proof.A[:]), common.BigIntsToBytes(proof.Z[:]), s, t, nil
+}
