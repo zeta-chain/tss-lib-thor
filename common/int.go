18decaff91be11b118906bd78581edf36d4f6310
Promethea Raschke <promethea.raschke@keep.network>
2023-05-23 12:48:53 +0100

    Add utility functions to int.go

--- common/int.go
+++ common/int.go
@@ -19,6 +19,19 @@ var (
 	two  = big.NewInt(2)
 )
 
+
+func Eq(x, y *big.Int) bool {
+	return x.Cmp(y) == 0
+}
+
+// return x + yz
+func AddMul(x, y, z *big.Int) *big.Int {
+	res := new(big.Int)
+	res.Mul(y, z)
+	res.Add(res, x)
+	return res
+}
+
 func ModInt(mod *big.Int) *modInt {
 	return (*modInt)(mod)
 }
@@ -51,10 +64,61 @@ func (mi *modInt) Exp(x, y *big.Int) *big.Int {
 	return new(big.Int).Exp(x, y, mi.i())
 }
 
+// return x * y^z % mi
+func (mi *modInt) MulExp(x, y, z *big.Int) *big.Int {
+	return mi.Mul(x, mi.Exp(y, z))
+}
+
+// return x^y * z^w % mi
+func (mi *modInt) ExpMulExp(x, y, z, w *big.Int) *big.Int {
+	return mi.Mul(mi.Exp(x, y), mi.Exp(z, w))
+}
+
 func (mi *modInt) ModInverse(g *big.Int) *big.Int {
 	return new(big.Int).ModInverse(g, mi.i())
 }
 
 func (mi *modInt) i() *big.Int {
 	return (*big.Int)(mi)
+}
+
+// Marshal the given bigint into bytes.
+// with the sign stored in the first byte and the absolute value in the rest.
+// `nil` or 0 is stored as the byte 0x00.
+// The sign byte is 0x00 for positive and 0x01 for negative.
+func MarshalSigned(i *big.Int) []byte {
+	if i == nil || Eq(i, big.NewInt(0)) {
+		return []byte{0}
+	}
+
+	// 0 = positive, 1 = negative
+	sign := make([]byte, 1)
+	if i.Sign() == 1 {
+		sign[0] = 0
+	} else {
+		sign[0] = 1
+	}
+
+	bs := i.Bytes()
+
+	return append(sign, bs...)
+}
+
+// Unmarshal a signed bigint from the given bytes.
+// Slices of length 1 are interpreted as 0;
+// in longer slices the first byte determines the sign
+// (0x00 is positive, anything else is negative)
+// and the remaining bytes contain the value of the bigint.
+func UnmarshalSigned(b []byte) *big.Int {
+	if len(b) == 1 {
+		return big.NewInt(0)
+	}
+
+	sign := b[0]
+	rest := b[1:]
+	i := new(big.Int).SetBytes(rest)
+	if sign != 0 {
+		i.Neg(i)
+	}
+	return i
 }
---
+++ common/int_test.go
@@ -0,0 +1,76 @@
+package common_test
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/bnb-chain/tss-lib/common"
+)
+
+func TestMarshalSigned(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal([]byte{0}, common.MarshalSigned(nil), "Nil should marshal to 0x00")
+	assert.Equal([]byte{0}, common.MarshalSigned(big.NewInt(0)), "0 should marshal to 0x00")
+	assert.Equal([]byte{0, 1}, common.MarshalSigned(big.NewInt(1)), "1 should marshal to 0x0001")
+	assert.Equal([]byte{0, 1, 1}, common.MarshalSigned(big.NewInt(257)), "257 should marshal to 0x000101")
+	assert.Equal([]byte{1, 1}, common.MarshalSigned(big.NewInt(-1)), "-1 should marshal to 0x0101")
+}
+
+func TestUnmarshalSigned(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.True(
+		big.NewInt(0).Cmp(common.UnmarshalSigned([]byte{0})) == 0,
+		"0x00 should unmarshal to 0",
+	)
+	assert.True(
+		big.NewInt(0).Cmp(common.UnmarshalSigned([]byte{1})) == 0,
+		"0x01 should unmarshal to 0",
+	)
+	assert.True(
+		big.NewInt(0).Cmp(common.UnmarshalSigned([]byte{255})) == 0,
+		"0xff should unmarshal to 0",
+	)
+
+	assert.True(
+		big.NewInt(0).Cmp(common.UnmarshalSigned([]byte{0, 0})) == 0,
+		"0x0000 should unmarshal to 0",
+	)
+	assert.True(
+		big.NewInt(0).Cmp(common.UnmarshalSigned([]byte{1, 0})) == 0,
+		"0x0100 should unmarshal to 0",
+	)
+	assert.True(
+		big.NewInt(0).Cmp(common.UnmarshalSigned([]byte{255, 0})) == 0,
+		"0xff00 should unmarshal to 0",
+	)
+
+	assert.True(
+		big.NewInt(1).Cmp(common.UnmarshalSigned([]byte{0, 1})) == 0,
+		"0x0001 should unmarshal to 1",
+	)
+	assert.True(
+		big.NewInt(-1).Cmp(common.UnmarshalSigned([]byte{1, 1})) == 0,
+		"0x0101 should unmarshal to -1",
+	)
+	assert.True(
+		big.NewInt(-1).Cmp(common.UnmarshalSigned([]byte{255, 1})) == 0,
+		"0xff01 should unmarshal to -1",
+	)
+
+	assert.True(
+		big.NewInt(255).Cmp(common.UnmarshalSigned([]byte{0, 255})) == 0,
+		"0x00ff should unmarshal to 255",
+	)
+	assert.True(
+		big.NewInt(-255).Cmp(common.UnmarshalSigned([]byte{1, 255})) == 0,
+		"0x01ff should unmarshal to -255",
+	)
+	assert.True(
+		big.NewInt(-255).Cmp(common.UnmarshalSigned([]byte{255, 255})) == 0,
+		"0xffff should unmarshal to -255",
+	)
+}
