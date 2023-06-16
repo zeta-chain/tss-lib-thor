f526493e41e7f2f2cdcd7f822f587ef87dad3a6d
Promethea Raschke <promethea.raschke@keep.network>
2023-06-16 09:59:31 +0100

    Simplify resharing round 5 indentation
    Instead of enclosing new committee members' code inside an if statement,
    return early on non-members. This is consistent with e.g. round 4, and
    reduces indentation by one level in the bulk of the function.

--- ecdsa/resharing/round_5_new_step_3.go
+++ ecdsa/resharing/round_5_new_step_3.go
@@ -23,97 +23,104 @@ func (round *round5) Start() *tss.Error {
 	round.started = true
 
 	round.allOldOK()
-	round.allNewOK()
 
 	Pi := round.PartyID()
 	i := Pi.Index
 
-	if round.IsNewCommittee() {
-		// 21.
-		// for this P: SAVE data
-		round.save.BigXj = round.temp.newBigXjs
-		round.save.ShareID = round.PartyID().KeyInt()
-		round.save.Xi = round.temp.newXi
-		round.save.Ks = round.temp.newKs
+	if !round.IsNewCommittee() {
+		if round.IsOldCommittee() {
+			round.input.Xi.SetInt64(0)
+			round.allNewOK()
+		}
+		round.end <- *round.save
+		return nil
+	}
 
-		// misc: build list of paillier public keys to save
-		for j, msg := range round.temp.dgRound2Message1s {
-			if j == i {
-				continue
+	// 21.
+	// for this P: SAVE data
+	round.save.BigXj = round.temp.newBigXjs
+	round.save.ShareID = round.PartyID().KeyInt()
+	round.save.Xi = round.temp.newXi
+	round.save.Ks = round.temp.newKs
+
+	// misc: build list of paillier public keys to save
+	for j, msg := range round.temp.dgRound2Message1s {
+		if j == i {
+			continue
+		}
+		r2msg1 := msg.Content().(*DGRound2Message1)
+		round.save.PaillierPKs[j] = r2msg1.UnmarshalPaillierPK()
+	}
+
+	Ps := round.NewParties().IDs()
+
+	type proofOut struct {
+		unWrappedErr error
+	}
+	chs := make([]chan proofOut, len(Ps))
+	for j, Pj := range Ps {
+		if common.Eq(Pi.KeyInt(), Pj.KeyInt()) {
+			continue
+		}
+		chs[j] = make(chan proofOut)
+	}
+	for j, Pj := range Ps {
+		if common.Eq(Pi.KeyInt(), Pj.KeyInt()) {
+			continue
+		}
+		go func(j int, ch chan<- proofOut) {
+			r4msg1 := round.temp.dgRound4Message1s[j].Content().(*DGRound4Message1)
+
+			FacProof := r4msg1.UnmarshalFactorProof()
+			pkN := round.save.PaillierPKs[j].N
+			NTilde := round.save.LocalPreParams.NTildei
+			H1i, H2i := round.save.LocalPreParams.H1i, round.save.LocalPreParams.H2i
+			ok, err := FacProof.FactorVerify(pkN, NTilde, H1i, H2i)
+			if err != nil {
+				ch <- proofOut{err}
 			}
-			r2msg1 := msg.Content().(*DGRound2Message1)
-			round.save.PaillierPKs[j] = r2msg1.UnmarshalPaillierPK()
-		}
-		
-		Ps := round.NewParties().IDs()
+			if !ok {
+				ch <- proofOut{errors.New("factor proof verify failed")}
+			}
+			FacProofTilde := r4msg1.UnmarshalFactorProofTilde()
+			NTildej := round.save.NTildej[j]
+			ok, err = FacProofTilde.FactorVerify(NTildej, NTilde, H1i, H2i)
+			if err != nil {
+				ch <- proofOut{err}
+			}
+			if !ok {
+				ch <- proofOut{errors.New("factor proof verify failed")}
+			}
+			// (9) handled above
+			ch <- proofOut{nil}
+		}(j, chs[j])
+	}
 
-		type proofOut struct {
-			unWrappedErr error
-		}
-		chs := make([]chan proofOut, len(Ps))
+	proofResults := make([]proofOut, len(Ps))
+	{
+		culprits := make([]*tss.PartyID, 0, len(Ps))
 		for j, Pj := range Ps {
 			if common.Eq(Pi.KeyInt(), Pj.KeyInt()) {
+				round.newOK[j] = true
 				continue
 			}
-			chs[j] = make(chan proofOut)
-		}
-		for j, Pj := range Ps {
-			if common.Eq(Pi.KeyInt(), Pj.KeyInt()) {
-				continue
+			proofResults[j] = <-chs[j]
+			if err := proofResults[j].unWrappedErr; err != nil {
+				culprits = append(culprits, Pj)
+			} else {
+				round.newOK[j] = true
 			}
-			go func(j int, ch chan<- proofOut) {
-				r4msg1 := round.temp.dgRound4Message1s[j].Content().(*DGRound4Message1)
-
-				FacProof := r4msg1.UnmarshalFactorProof()
-				pkN := round.save.PaillierPKs[j].N
-				NTilde := round.save.LocalPreParams.NTildei
-				H1i, H2i := round.save.LocalPreParams.H1i, round.save.LocalPreParams.H2i
-				ok, err := FacProof.FactorVerify(pkN, NTilde, H1i, H2i)
-				if err != nil {
-					ch <- proofOut{err}
-				}
-				if !ok {
-					ch <- proofOut{errors.New("factor proof verify failed")}
-				}
-				FacProofTilde := r4msg1.UnmarshalFactorProofTilde()
-				NTildej := round.save.NTildej[j]
-				ok, err = FacProofTilde.FactorVerify(NTildej, NTilde, H1i, H2i)
-				if err != nil {
-					ch <- proofOut{err}
-				}
-				if !ok {
-					ch <- proofOut{errors.New("factor proof verify failed")}
-				}
-				// (9) handled above
-				ch <- proofOut{nil}
-			}(j, chs[j])
 		}
-
-		proofResults := make([]proofOut, len(Ps))
-		{
-			culprits := make([]*tss.PartyID, 0, len(Ps))
-			for j, Pj := range Ps {
-				if common.Eq(Pi.KeyInt(), Pj.KeyInt()) {
+		var multiErr error
+		if len(culprits) > 0 {
+			for _, proofResult := range proofResults {
+				if proofResult.unWrappedErr == nil {
 					continue
 				}
-				proofResults[j] = <-chs[j]
-				if err := proofResults[j].unWrappedErr; err != nil {
-					culprits = append(culprits, Pj)
-				}
+				multiErr = multierror.Append(multiErr, proofResult.unWrappedErr)
 			}
-			var multiErr error
-			if len(culprits) > 0 {
-				for _, proofResult := range proofResults {
-					if proofResult.unWrappedErr == nil {
-						continue
-					}
-					multiErr = multierror.Append(multiErr, proofResult.unWrappedErr)
-				}
-				return round.WrapError(multiErr, culprits...)
-			}
+			return round.WrapError(multiErr, culprits...)
 		}
-	} else if round.IsOldCommittee() {
-		round.input.Xi.SetInt64(0)
 	}
 
 	round.end <- *round.save
